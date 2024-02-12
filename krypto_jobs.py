555b091e08b34172803236a53f1434b8c33544cc
gouldrussell <144621362+gouldrussell@users.noreply.github.com>
2024-02-12 16:15:50 -0500

    Add files via upload

--- krypto_jobs.py
+++ krypto_jobs.py
@@ -173,9 +173,9 @@ st.sidebar.write(account.address)
 # @TODO
 # Call `get_balance` function and pass it your account address
 # Write the returned ether balance to the sidebar
-ether = get_balance(w3, account.address)
+ether_amount = get_balance(w3, account.address)
 st.sidebar.markdown("## Your Balance of Ether")
-st.sidebar.markdown(ether)
+st.sidebar.markdown(ether_amount)
 st.sidebar.markdown("------------------------")
 
 ##########################################
@@ -267,12 +267,12 @@ st.sidebar.markdown("## Total Wage in Ether")
 # Calculate total `wage` for the candidate by multiplying the candidate’s hourly
 # rate from the candidate database (`candidate_database[person][3]`) by the
 # value of the `hours` variable
-wage = candidate_database[person][3] * hours
+total_wage = candidate_database[person][3] * hours
 
 # @TODO
 # Write the `wage` calculation to the Streamlit sidebar
 st.sidebar.markdown("## Candidates hourly wage")
-st.sidebar.markdown(wage)
+st.sidebar.markdown(total_wage)
 st.sidebar.markdown("--------------------------")
 
 ##########################################
@@ -300,7 +300,7 @@ if st.sidebar.button("Send Transaction"):
     # Call the `send_transaction` function and pass it 3 parameters:
     # Your `account`, the `candidate_address`, and the `wage` as parameters
     # Save the returned transaction hash as a variable named `transaction_hash`
-    transaction_hash = send_transaction(w3, account, candidate_address, wage)
+    transaction_hash = send_transaction(w3, account, candidate_address, total_wage)
 
     # Markdown for the transaction hash
     st.sidebar.markdown("## Validated Transaction Hash")
